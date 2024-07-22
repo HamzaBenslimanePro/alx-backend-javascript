3bc06204c23334b5d8ed8775f64b2dc65e667ce3
HamzaBenslimane <hamzasamiben@gmail.com>
2024-07-22 18:32:32 +0000

    just another commit

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,18 +1,9 @@
 export default function iterateThroughObject(reportWithIterator) {
-  const array = reportWithIterator;
-  let str = '';
+  const employees = [];
 
-  for (let i = 0; i < array.length; i += 1) {
-    if (array[i] !== '') {
-      if (str === '') {
-        str += array[i];
-      } else if (i === array.length - 1) {
-        str += ` | ${array[i]}`;
-      } else {
-        str += ` | ${array[i]} `;
-      }
-    }
+  for (const employee of reportWithIterator) {
+    employees.push(employee);
   }
 
-  return str;
+  return employees.join(' | ');
 }
